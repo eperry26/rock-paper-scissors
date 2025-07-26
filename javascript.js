9b5669492ce05c4a6ac6bc11b80dcace3c282e17
Elizabeth Perry <110440713+eperry26@users.noreply.github.com>
2025-07-25 20:32:13 -0400

    Create function to get human choice & play round

--- javascript.js
+++ javascript.js
@@ -11,14 +11,16 @@ function getComputerChoice() {
 
     let comp_choice = ""
     let randNum = Math.floor((Math.random()*100))+1
-    console.log(randNum)
+    // console.log(randNum)
 
     if (randNum >=0 & randNum <= 33) {
         comp_choice = "rock"
     }
+
     else if (randNum >= 34 & randNum <= 66) {
         comp_choice = "paper"
     }
+
     else {
         comp_choice = "scissors"
     }
@@ -28,5 +30,60 @@ function getComputerChoice() {
 
 }
 
-// Stopping point: keep getting only rock
-console.log(getComputerChoice())
+function getHumanChoice () {
+    // User input to get human choice. Use prompt()
+    human_choice = prompt("Rock, Paper, or Scissors?: ")
+    return human_choice
+}
+
+
+
+function playGame () {
+    // Initialize score variables
+    let humanScore = 0
+    let computerScore = 0
+    function playRound(humanChoice, computerChoice) {
+    // Make humanChoice case insensitive
+
+    if (humanChoice.toLowerCase() === computerChoice) {
+        console.log("You tied!")
+    }
+
+    else if (humanChoice.toLowerCase() === 'rock' & computerChoice === 'paper') {
+        console.log("Paper beats rock, computer scores a point!")
+        computerScore++
+    }
+
+    else if (humanChoice.toLowerCase() === 'paper' & computerChoice === 'rock') {
+        console.log("Paper beats rock, human scores a point!")
+        humanScore++
+    }
+
+    else if (humanChoice.toLowerCase() === 'paper' & computerChoice === 'scissors') {
+        console.log("Scissors beats paper, computer scores a point!")
+        computerScore++
+    }
+
+    else if (humanChoice.toLowerCase() === 'scissors' & computerChoice === 'paper') {
+        console.log("Scissors beats paper, human scores a point!")
+        humanScore++
+    }
+
+    else if (humanChoice.toLowerCase() === 'scissors' & computerChoice === 'rock') {
+        console.log("Rock beats scissors, computer scores a point!")
+        computerScore++
+    }
+
+    else if (humanChoice.toLowerCase() === 'rock' & computerChoice === 'scissors') {
+        console.log("Rock beats scissors, human scores a point!")
+        humanScore++
+    }
+
+}
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
